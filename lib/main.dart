a3fdbcb350d3195818ddb4c86e1fcc406774124e
Tania Joseph <taniajoseph37@gmail.com>
2020-06-05 01:35:40 +0530

    Know your future.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,53 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue.shade400,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text('Ask Me Anything'),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+
+  int rand = 1;
+
+  void change(){
+    setState(() {
+      rand = Random().nextInt(5) + 1;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: FlatButton(
+          onPressed: (){
+            change();
+          },
+          child: Image.asset('images/ball$rand.png'),
+        ),
+      ),
+    );
+  }
+}
+
+
